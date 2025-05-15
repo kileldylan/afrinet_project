43ef07b141e6682f9f8cf46fb9c83afef126d5c4
kileldylan <kileldylan@gmail.com>
2025-05-15 21:00:21 +0300

    configure with the live backend url

--- afrinet_frontend/src/App.jsx
+++ afrinet_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import AdminDashboard from './pages/admin_dashboard';
 import Dashboard from './pages/dashboard';
+import AdminDashboard from './pages/admin_dashboard';
 
 function App() {
   return (
