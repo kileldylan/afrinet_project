a11b0fe47864155677c824966791db533aadc53c
kileldylan <kileldylan@gmail.com>
2025-05-15 21:19:46 +0300

    fixed import

--- afrinet_frontend/src/App.jsx
+++ afrinet_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/dashboard.jsx';
-import AdminDashboard from './pages/admin_dashboard.jsx';
+import Dashboard from './pages/dashboard';
+import AdminDashboard from './pages/admin_dashboard';
 
 
 function App() {
